19753e34d212280206fc743b2a41bd8d6894e89b
Milan Burmaja <burmajam@gmail.com>
2013-01-17 14:48:48 +0100

    created new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Restler::Application.config.secret_token = '1e77444a3d658f64dd88b271f56ea84c696d14f800c4f665ed35a708d22a282f5d19c0638e01ae22d8794930c621286452d72839fcc36ab067e5a72d92704367'
