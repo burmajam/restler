04204921deb3971f27f34a241bea25109b4f8ecb
Milan Burmaja <burmajam@gmail.com>
2013-01-24 22:23:29 +0100

    put shoulda matchers in contexts

--- lib/gems/define_rest/spec/define_rest/header_spec.rb
+++ lib/gems/define_rest/spec/define_rest/header_spec.rb
@@ -2,6 +2,8 @@ require_relative '../activerecord_helper'
 require_relative '../../../define_rest/lib/define_rest/header'
 
 describe DefineRest::Header do
-  it { should respond_to :key }
-  it { should respond_to :value }
+  context 'fields:' do
+    it { should respond_to :key }
+    it { should respond_to :value }
+  end
 end
--- lib/gems/define_rest/spec/define_rest/parameter_spec.rb
+++ lib/gems/define_rest/spec/define_rest/parameter_spec.rb
@@ -2,11 +2,15 @@ require_relative '../activerecord_helper'
 require_relative '../../../define_rest/lib/define_rest/parameter'
 
 describe DefineRest::Parameter do
-  it { should respond_to :name }
-  it { should respond_to :description }
-  it { should respond_to :presence }
-  it { should respond_to :example }
-  it { should respond_to :quoted_in_json? }
+  context 'fields:' do
+    it { should respond_to :name }
+    it { should respond_to :description }
+    it { should respond_to :presence }
+    it { should respond_to :example }
+    it { should respond_to :quoted_in_json? }
+  end
 
-  it { should ensure_inclusion_of(:presence).in_array(['optional', 'mandatory']) }
+  context 'validations:' do
+    it { should ensure_inclusion_of(:presence).in_array(['optional', 'mandatory']) }
+  end
 end
