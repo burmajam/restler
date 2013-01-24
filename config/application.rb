8e6ea9173f1966332d0106d99b873258f1509fc2
Milan Burmaja <burmajam@gmail.com>
2013-01-24 14:32:40 +0100

    Add gem migration path to rails app path and generate db for it

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,8 @@ module Restler
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
 
+    config.paths['db/migrate'] << 'lib/gems/define_rest/db/migrate'
+
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
